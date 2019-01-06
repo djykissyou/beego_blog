50f3d52f9bdb5804020db3c30c00b141af18fb4c
jinyong <zhaogeppt@qq.com>
2019-01-06 22:55:23 +0800

    后台管理面板自适应（iframe内点击无效后期解决）

---
+++ static/js/admin-global.js
@@ -0,0 +1,90 @@
+﻿/**
+ * 对layui进行全局配置
+ */
+layui.config({
+    base: '/static/js/'
+});
+
+layui.use('form', function() {
+    var $ = layui.jquery,
+        form = layui.form();
+});
+
+/**
+ * 初始化整个系统骨架
+ */
+layui.use(['cms'], function() {
+    var cms = layui.cms('left-nav', 'top-tab');
+    cms.addNav([
+        { id: 1, pid: 0, node: '<span style=" font-size: 16px"><i class="layui-icon">&#xe620;</i> 系统管理</span>', url: '#' },
+        { id: 7, pid: 1, node: '&nbsp;&nbsp;&nbsp;系统设置', url: '/admin/config.html' },
+        { id: 2, pid: 0, node: '<span style=" font-size: 16px"><i class="layui-icon">&#xe63c;</i> 内容管理</span>', url: '#' },
+        { id: 3, pid: 2, node: '&nbsp;&nbsp;&nbsp;分类管理', url: '/admin/category.html' },
+        { id: 5, pid: 2, node: '&nbsp;&nbsp;&nbsp;博文列表', url: '/admin/index.html' },
+        { id: 6, pid: 2, node: '&nbsp;&nbsp;&nbsp;博文添加', url: '/admin/article.html' },
+    ], 0, 'id', 'pid', 'node', 'url');
+    cms.bind(60 + 41 + 20 + 44); //头部高度 + 顶部切换卡标题高度 + 顶部切换卡内容padding + 底部高度
+    cms.clickLI(5);
+
+});
+
+function addTab(title, src, id,closeId){
+    if(closeId){
+//					debugger;
+        closeTab(closeId);
+    }
+    layui.use(['cms'], function() {
+        var cms = layui.cms('left-nav', 'top-tab');
+        cms.addTab(title,src,id);
+    });
+}
+
+function closeTab(id,refreshId){
+    layui.use(['cms'], function() {
+        var cms = layui.cms('left-nav', 'top-tab');
+        cms.closeTab(id,refreshId);
+    });
+}
+layui.use(['element', 'layer', 'util', 'form'], function () {
+    var $ = layui.jquery;
+
+    //blog-body和blog-footer点击事件，用来关闭百度分享和类别导航
+    $('.layui-body,.layui-footer').click(function () {
+        panelOut();
+    });
+    //管理面板导航开关点击事件
+    $('.panel-toggle').click(function (e) {
+        e.stopPropagation();    //阻止事件冒泡
+        panelIn();
+    });
+    //管理面板导航点击事件，用来关闭类别导航
+    $('.admin-panel').click(function () {
+        panelOut();
+    });
+    //具体管理面板点击事件
+    $('.admin-panel > a').click(function (e) {
+        e.stopPropagation(); //阻止事件冒泡
+    });
+
+
+    //显示管理面板导航
+    function panelIn() {
+        $('.panel-toggle').addClass('layui-hide');
+        $('.admin-panel').unbind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+
+        $('.admin-panel').removeClass('panelOut');
+        $('.admin-panel').addClass('panelIn');
+        $('.admin-panel').addClass('layui-show');
+    }
+    //隐藏管理面板导航
+    function panelOut() {
+        $('.admin-panel').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $('.admin-panel').removeClass('layui-show');
+            $('.panel-toggle').removeClass('layui-hide');
+        });
+
+        $('.admin-panel').removeClass('panelIn');
+        $('.admin-panel').addClass('panelOut');
+    }
+
+});
--- views/admin/main.tpl
+++ views/admin/main.tpl
@@ -8,6 +8,8 @@
     <meta name="Description" content="">
     <title>后台管理系统</title>
     <link rel="stylesheet" href="/static/plug/layui/css/layui.css">
+    <link href="/static/plug/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/static/css/admin.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 
@@ -43,12 +45,14 @@
     </div>
 
     <!--侧边栏 -->
-    <div class="layui-side layui-bg-black">
+    <div class="layui-side layui-bg-black admin-panel">
         <div class="layui-side-scroll">
             <ul class="layui-nav layui-nav-tree " lay-filter="left-nav" style="border-radius: 0;">
             </ul>
         </div>
     </div>
+    <div class="panel-toggle"><i class="fa fa-chevron-right"></i></div>
+
 
     <!-- 主体 -->
     <div class="layui-body">
@@ -67,56 +71,7 @@
 
 <script src="/static/plug/layui/lay/lib/jquery.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" src="/static/plug/layui/layui.js"></script>
-<script type="text/javascript">
-    /**
-     * 对layui进行全局配置
-     */
-    layui.config({
-        base: '/static/js/'
-    });
-
-    layui.use('form', function() {
-        var $ = layui.jquery,
-            form = layui.form();
-    });
-
-    /**
-     * 初始化整个系统骨架
-     */
-    layui.use(['cms'], function() {
-        var cms = layui.cms('left-nav', 'top-tab');
-        cms.addNav([
-            { id: 1, pid: 0, node: '<span style=" font-size: 16px"><i class="layui-icon">&#xe620;</i> 系统管理</span>', url: '#' },
-            { id: 7, pid: 1, node: '&nbsp;&nbsp;&nbsp;系统设置', url: '/admin/config.html' },
-            { id: 2, pid: 0, node: '<span style=" font-size: 16px"><i class="layui-icon">&#xe63c;</i> 内容管理</span>', url: '#' },
-            { id: 3, pid: 2, node: '&nbsp;&nbsp;&nbsp;分类管理', url: '/admin/category.html' },
-            { id: 5, pid: 2, node: '&nbsp;&nbsp;&nbsp;博文列表', url: '/admin/index.html' },
-            { id: 6, pid: 2, node: '&nbsp;&nbsp;&nbsp;博文添加', url: '/admin/article.html' },
-        ], 0, 'id', 'pid', 'node', 'url');
-        cms.bind(60 + 41 + 20 + 44); //头部高度 + 顶部切换卡标题高度 + 顶部切换卡内容padding + 底部高度
-        cms.clickLI(5);
-
-    });
-
-    function addTab(title, src, id,closeId){
-        if(closeId){
-//					debugger;
-            closeTab(closeId);
-        }
-        layui.use(['cms'], function() {
-            var cms = layui.cms('left-nav', 'top-tab');
-            cms.addTab(title,src,id);
-        });
-    }
-
-    function closeTab(id,refreshId){
-        layui.use(['cms'], function() {
-            var cms = layui.cms('left-nav', 'top-tab');
-            cms.closeTab(id,refreshId);
-        });
-    }
-
-</script>
+<script src="/static/js/admin-global.js"></script>
 </body>
 
 </html>
